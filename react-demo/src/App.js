ce211047659a42c1d66988d6ed83953abcb85d60
kapilnagi <kapilnagi007@gmail.com>
2026-06-04 18:36:35 +0530

    Update App.js

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>This is my DOCKER CICD</h1>
+        <h2>This is my second text</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
